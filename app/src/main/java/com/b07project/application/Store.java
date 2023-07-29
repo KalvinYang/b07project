5c5f6c366924538290c0faa8d198bbf1ac7bfec3
vivianz1 <vivianzhu120@gmail.com>
2023-07-28 23:21:02 -0400

    Created UI for Login Page

--- app/src/main/java/com/b07project/application/Store.java
+++ app/src/main/java/com/b07project/application/Store.java
@@ -10,22 +10,22 @@ import java.util.List;
 
 class Store {
     String brand;
-    List<Item> items = null;
-
-    Store(String brand) {
-        this.brand = brand;
-    }
-
-    void addItem(Item i) {
-        // if item list is not made yet,
-        if (items == null) {
-            items = new ArrayList<Item>();
-        }
-        items.add(i);
-    }
-
-    // To Do
-    void removeItem(Item i) {
-        return;
-    }
+//    List<Item> items = null;
+//
+//    Store(String brand) {
+//        this.brand = brand;
+//    }
+//
+//    void addItem(Item i) {
+//        // if item list is not made yet,
+//        if (items == null) {
+//            items = new ArrayList<Item>();
+//        }
+//        items.add(i);
+//    }
+//
+//    // To Do
+//    void removeItem(Item i) {
+//        return;
+//    }
 }
