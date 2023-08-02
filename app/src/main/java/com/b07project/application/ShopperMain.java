181155573c6386a92615dc9b7ab1ebbfae18e64a
Masahisa Sekita <masahisa.sekita@mail.utoronto.ca>
2023-08-02 17:07:40 -0400

    Merged to Ui of StoreOwnerMain and ShopperMain to have fragments of OrdersFragment, MyCartFragment, and MyOrdersFragment

--- app/src/main/java/com/b07project/application/ShopperMain.java
+++ app/src/main/java/com/b07project/application/ShopperMain.java
@@ -1,29 +1,56 @@
 package com.b07project.application;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 import com.google.firebase.auth.FirebaseAuth;
 
 public class ShopperMain extends AppCompatActivity {
 
+    Button MyCartFragmentBtn, MyOrdersFragmentBtn, ShopsFragmentBtn;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_shopper_main);
+
+        MyCartFragmentBtn = findViewById(R.id.MyCartButton);
+        MyOrdersFragmentBtn = findViewById(R.id.OwnerOrders);
+        ShopsFragmentBtn = findViewById(R.id.ShopButton);
+
+        MyCartFragmentBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                replaceFragment(new MyCartFragment());
+            }
+        });
+
+        MyOrdersFragmentBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                replaceFragment(new MyOrdersFragment());
+            }
+        });
+
+    }
+
+    private void replaceFragment(Fragment fragment){
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.ShopperFrameLayout,fragment);
+        fragmentTransaction.commit();
     }
 
     public void fromShopperMainToMain(View view){
         Intent intent = new Intent(this, MainActivity.class);
         startActivity(intent);
     }
-
-    /*public void fromShopperMainToShopperShops(View view){
-        Intent intent = new Intent(this, ShopperShopsActivity.class);
-        startActivity(intent);
-    }*/
 
 }
--- app/src/main/java/com/b07project/application/StoreOwnerMain.java
+++ app/src/main/java/com/b07project/application/StoreOwnerMain.java
@@ -1,17 +1,38 @@
 package com.b07project.application;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 public class StoreOwnerMain extends AppCompatActivity {
 
+    Button OrdersFragmentBtn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_store_owner_main);
+
+        OrdersFragmentBtn = findViewById(R.id.OwnerOrders);
+
+        OrdersFragmentBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                replaceFragment(new OrdersFragment());
+            }
+        });
+    }
+
+    private void replaceFragment(Fragment fragment){
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.StoreOwnerFrameLayout,fragment);
+        fragmentTransaction.commit();
     }
 
 }
