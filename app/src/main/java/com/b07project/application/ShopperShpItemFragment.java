4a8cff0dd17e3f648e132dfa11910cfc4db49105
GursimarSingh1 <gursimar654@gmail.com>
2023-08-08 18:56:31 -0400

    ShopperShpItemFragment.java now displays the item details

--- app/src/main/java/com/b07project/application/ShopperShpItemFragment.java
+++ app/src/main/java/com/b07project/application/ShopperShpItemFragment.java
@@ -14,6 +14,12 @@ import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.TextView;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
+
 /**
  * A simple {@link Fragment} subclass.
  * Use the {@link ShopperShpItemFragment#newInstance} factory method to
@@ -29,6 +35,8 @@ public class ShopperShpItemFragment extends Fragment {
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
+
+    DatabaseReference ref = MainActivity.db.getReference("Item");
 
     public ShopperShpItemFragment() {
         // Required empty public constructor
@@ -74,11 +82,40 @@ public class ShopperShpItemFragment extends Fragment {
         TextView ShopViewItemBrand = view.findViewById(R.id.shopItemBrandText);
         TextView ShopViewItemDescription = view.findViewById(R.id.shopItemDecriptionText);
         TextView ShopViewItemSpecification = view.findViewById(R.id.shopItemSpecification);
+        TextView ShopViewItemPrice = view.findViewById(R.id.shopItemPriceText);
         Button addToCartButton = view.findViewById(R.id.ItemAddItemToCartButton);
         Button backButton = view.findViewById(R.id.BackToShopViewButton);
 
-        ShopViewItemName.setText(mParam1);
-        ShopViewItemBrand.setText(mParam2);
+        //mparam1 = name , mparam2 = brand
+
+
+        Query query = ref.orderByChild("name").equalTo(mParam1);
+
+        query.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                if (snapshot.exists()) {
+                    for ( DataSnapshot snapshot1 : snapshot.getChildren()) {
+
+                        if (mParam2.equals(snapshot1.child("brand").getValue(String.class))) {
+                            ShopViewItemName.setText(mParam1);
+                            ShopViewItemBrand.setText(mParam2);
+                            ShopViewItemDescription.setText(snapshot1.child("description").getValue(String.class));
+                            ShopViewItemSpecification.setText(snapshot1.child("specifications").getValue(String.class));
+                            ShopViewItemPrice.setText(Float.toString(snapshot1.child("price").getValue(float.class)));
+
+
+
+                        }
+                    }
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+            }
+        });
+
 
         backButton.setOnClickListener(new View.OnClickListener() {
             @Override
